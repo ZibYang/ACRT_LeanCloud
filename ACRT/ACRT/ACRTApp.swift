834816bd4af53aeebd81ffa42ffdcac16af0a99b
ZibYang <996478975@qq.com>
2021-10-29 17:22:43 +0800

    update

--- ACRT/ACRT/ACRTApp.swift
+++ ACRT/ACRT/ACRTApp.swift
@@ -15,9 +15,6 @@
 //  Copyright © 2021 Augmented City Reality Toolkit. All Right Reserved.
 
 // [Coredata] reference: https://www.cnblogs.com/yajunLi/p/6600700.html
-// [Firebase] reference 1: https://www.youtube.com/watch?v=ySETHKCELhU
-//            reference 2: https://firebase.google.com/docs/auth/ios/anonymous-auth?hl=en
-
 // [leanCloud] reference: https://leancloud.cn/docs/sdk_setup-swift.html#hash-1726498857
 
 import SwiftUI
@@ -25,11 +22,7 @@ import LeanCloud
 
 @main
 struct ACRTApp: App {
-    
-    // [Deprecated] MARK: FireBase
-    // @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate // Deprecated
     init(){
-        // FirebaseApp.configure()
         // MARK: leanCloud
         do {
             try LCApplication.default.set(
@@ -46,16 +39,6 @@ struct ACRTApp: App {
         WindowGroup {
             // MARK: View
             MainView()
-            // SignInWithEmailAndPasswordView()
         }
     }
 }
-
-// [Deprecated]
-//class AppDelegate: NSObject, UIApplicationDelegate{
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool{
-//        FirebaseApp.configure()
-//
-//        return true
-//    }
-//}
