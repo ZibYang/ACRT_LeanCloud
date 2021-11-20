8b4b74f44e1aafb85adfbd29e7b136d16e6f9291
Bao <b757508036@126.com>
2021-11-20 19:41:44 +0800

    fix the transformation bug when upload

--- ACRT/ACRT/ViewModel/ARViewPageViewModels/PersistenceHelperViewModel.swift
+++ ACRT/ACRT/ViewModel/ARViewPageViewModels/PersistenceHelperViewModel.swift
@@ -40,68 +40,10 @@ class PersistenceHelperViewModel: ObservableObject {
     func uploadScene(for arView: CustomARView, at anchorEntities: [AnchorEntity], with usrname : String, poseARKitToW : simd_float4x4 ) {
         print("Save scene to local filesystem.")
         
-//        DEPRECATED MODE
-//         find anchorentity -> add entity
-//            check if scene is already exist
-//            let query = LCQuery(className: "Scene")
-//            query.whereKey("scene_name", .equalTo("default"))
-//            _ = query.find { result in
-//                switch result {
-//                case .success(objects: let scenes):
-//                    var object_id: LCString? = nil
-//                    if scenes.count > 0 {
-//                        object_id = scenes[0].objectId
-//                        print("[LH] \(String(describing: object_id))")
-//                    } else {
-//                        print("[LH] no query scene in database")
-//                    }
-//
-//                    var scene = LCObject(className: "Scene")
-//                    if object_id != nil {
-//                        scene = LCObject(className: "Scene", objectId: object_id!)
-//                    }
-//                    do {
-//                        try scene.set("creator", value: usrname)
-//                        try scene.set("scene_name", value: "default")
-//
-//                        var object_names: [String] = []
-//                        var object_poses: [String] = []
-//
-//                        for anchorEntity in anchorEntities {
-//                            if AnchorIdentifierHelper.decode(identifier: anchorEntity.name)[0] == usrname {
-//                                let poseWToAnchor = poseWToARKit * anchorEntity.transformMatrix(relativeTo: nil)
-//                                // save username , anchor.name , poseWToAnchor
-//                                print("[LH] append \(anchorEntity.name) ")
-//                                object_names.append(anchorEntity.name)
-//                                object_poses.append(poseWToAnchor.debugDescription)
-//                            }
-//                        }
-//                        try scene.set("object_names", value: object_names)
-//                        try scene.set("object_poses", value: object_poses)
-//
-//                        _ = scene.save { result in
-//                            switch result {
-//                            case .success:
-//                                // 成功保存之后，执行其他逻辑
-//                                break
-//                            case .failure(error: let error):
-//                                // 异常处理
-//                                print("[LH] \(error)")
-//                            }
-//                        }
-//                    } catch {
-//                        print("[LH] \(error)")
-//                    }
-//                case .failure(error: let error):
-//                    print("[LH] \(error)")
-//                }
-//            }
-
-        
         var objects: [LCObject] = []
         for anchorEntity in anchorEntities {
             if AnchorIdentifierHelper.decode(identifier: anchorEntity.name)[0] == usrname {
-                let poseWToAnchor = poseWToARKit * anchorEntity.transformMatrix(relativeTo: nil)
+                let poseWToAnchor = poseARKitToW.inverse * anchorEntity.transformMatrix(relativeTo: nil)
                 // save username , anchor.name , poseWToAnchor
                 let object = LCObject(className: "Objects")
                 do {
@@ -129,44 +71,6 @@ class PersistenceHelperViewModel: ObservableObject {
     func downloadScene(poseARKitToW : simd_float4x4) -> [ModelAnchor]{
         print("Load scene from local filesystem.")
         // return models which is appended into confirmedModel
-    
-//        DEPRECATED MODE
-//        let query = LCQuery(className: "Scene")
-//        query.whereKey("scene_name", .equalTo("default"))
-//        _ = query.find { result in
-//            switch result {
-//            case .success(objects: let scenes):
-//                if scenes.count > 0 {
-//                    let scene = scenes[0]
-//                    let object_names = scene.get("object_names") as! LCArray
-//                    let object_poses = scene.get("object_poses") as! LCArray
-//
-//
-//                    for i in 0..<object_names.value.count {
-//                        let obeject_name: String = (object_names[i] as! LCString).value
-//                        let object_pose: String = (object_poses[i] as! LCString).value
-//                        print("[LH] \(obeject_name) \(object_pose)")
-//
-//                        let name = AnchorIdentifierHelper.decode(identifier: obeject_name)[1]
-//                        print("[LH] \(name)")
-//                        guard let trans = simd_float4x4(object_pose) else {
-//                            return
-//                        }
-//                        let poseARKitToModel = poseWToARKit.inverse * trans
-//                        print("[LH] \(String(describing: poseARKitToModel))")
-//
-//                        let anchor = ModelAnchor(modelName: name, transform : poseARKitToModel, anchorName: obeject_name)
-//                        self.anchors.append(anchor)
-//                    }
-//
-//                } else {
-//                    print("[LH] no query scene in database")
-//                }
-//
-//            case .failure(error: let error):
-//                print("[LH] \(error)")
-//            }
-//        }
         
         let query = LCQuery(className: "Objects")
         query.whereKey("scene_name", .equalTo("default"))
@@ -185,7 +89,7 @@ class PersistenceHelperViewModel: ObservableObject {
                         guard let trans = simd_float4x4(object_pose) else {
                             return
                         }
-                        let poseARKitToModel = poseWToARKit.inverse * trans
+                        let poseARKitToModel = poseARKitToW * trans
                         print("[LH] \(String(describing: poseARKitToModel))")
 
                         let anchor = ModelAnchor(modelName: name, transform : poseARKitToModel, anchorName: object_name)
